1de4a12b75cd0deaec663b4dea939ad21d30ce5c
Madirex <angel@madirex.com>
2024-05-12 00:42:25 +0200

    added admin panel at app navbar

--- src/components/AppNavbar.vue
+++ src/components/AppNavbar.vue
@@ -1,12 +1,12 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
-      <a class="navbar-brand" href="/">  
-        <img src="/favicon.ico" alt="App Logo" class="d-inline-block align-text-top"/>
+      <a class="navbar-brand" href="/">
+        <img src="/favicon.ico" alt="App Logo" class="d-inline-block align-text-top" />
       </a>
 
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-              aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
 
@@ -18,17 +18,27 @@
           <li class="nav-item">
             <router-link to="/about" class="nav-link">Acerca de</router-link>
           </li>
-          <li class="nav-item">
-            <router-link to="/categories" class="nav-link">Categorías</router-link>
-          </li>
+          <div v-if="isAdmin">
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown"
+                aria-expanded="false">
+                Administración
+              </a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li class="nav-item">
+                  <router-link to="/categories" class="nav-link">Categorías</router-link>
+                </li>
+              </ul>
+            </li>
+          </div>
         </ul>
-          <div v-if="isAuthTokenAvailable">
-            <router-link to="/" class="nav-link" @click="logout">Cerrar sesión</router-link>
-          </div>
-          <div v-else>
-            <router-link to="/login" class="nav-link me-4">Iniciar sesión</router-link>
-            <router-link to="/signup" class="nav-link">Registro</router-link>
-          </div>
+        <div v-if="isAuthTokenAvailable">
+          <router-link to="/" class="nav-link" @click="logout">Cerrar sesión</router-link>
+        </div>
+        <div v-else>
+          <router-link to="/login" class="nav-link me-4">Iniciar sesión</router-link>
+          <router-link to="/signup" class="nav-link">Registro</router-link>
+        </div>
       </div>
     </div>
   </nav>
@@ -45,6 +55,9 @@ export default {
   computed: {
     isAuthTokenAvailable() {
       return this.authToken !== null;
+    },
+    isAdmin() {
+      return this.authToken !== null && localStorage.getItem('isAdmin') === 'true';
     }
   },
   watch: {
