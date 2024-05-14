876fb661c4f185533b44bd25939106e9d0ddac2c
Madirex <angel@madirex.com>
2024-05-14 09:14:20 +0200

    added custom favicon

--- src/components/AppNavbar.vue
+++ src/components/AppNavbar.vue
@@ -2,7 +2,7 @@
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
       <a class="navbar-brand" href="/">
-        <img src="/favicon.ico" alt="App Logo" class="d-inline-block align-text-top" />
+        <img src="/favicon.png" alt="App Logo" class="d-inline-block align-text-top" width="50" />
       </a>
 
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
