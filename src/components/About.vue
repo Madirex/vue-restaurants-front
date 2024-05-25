b59711ec232ed7a4f9e839f1541a35dcaa59d6b1
Madirex <angel@madirex.com>
2024-05-25 21:58:44 +0200

    implemented about page

---
+++ src/components/About.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="container mt-4">
+    <h2>Acerca de</h2>
+    <p>Web gestora de restaurantes y reservas. Hay diferentes restaurantes registrados. Cada restaurante tiene un menú con los diferentes platos disponibles. Puedes realizar una reserva en el restaurante a tu elección, elegir la mesa y la cantidad de sillas que quieres reservar junto a los diferentes platos. ¡De esa manera tendrás asegurado tu sitio en los restaurantes y tendremos tus platos preparados justo a tiempo!</p>
+  </div>
+</template>
